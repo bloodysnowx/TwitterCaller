cfe69dad9550338ad0f04db5bc56e3d1744ec799
bloodysnowx <bloodysnowx@gmail.com>
2014-11-30 15:18:43 +0900

    画像をシェアできるようにした 呼び出す先をtwitterに制限

--- app/src/main/java/com/bloodysnow/twittercaller/TweetActivity.java
+++ app/src/main/java/com/bloodysnow/twittercaller/TweetActivity.java
@@ -2,14 +2,21 @@ package com.bloodysnow.twittercaller;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+
+import java.io.File;
 
 public class TweetActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        tweet(intent == null ? null : intent.getStringExtra(Intent.EXTRA_TEXT));
+        String text = (intent == null || !intent.hasExtra(Intent.EXTRA_TEXT)) ? null : intent.getStringExtra(Intent.EXTRA_TEXT);
+        String textureUrl = (intent == null || !intent.hasExtra(Intent.EXTRA_STREAM)) ? null : intent.getStringExtra(Intent.EXTRA_STREAM);
+        tweet(text, textureUrl);
     }
 
     @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -18,10 +25,19 @@ public class TweetActivity extends Activity {
         finish();
     }
 
-    private void tweet(String text) {
+    private void tweet(String text, String textureUrl) {
         Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
+        intent.setType(textureUrl == null ? "text/plain" : "image/png");
         intent.putExtra(Intent.EXTRA_TEXT, text);
+        if(textureUrl != null) intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(textureUrl)));
+
+        for(ResolveInfo resolveInfo : getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)) {
+            if(resolveInfo.activityInfo.name.contains("twitter")) {
+                intent.setClassName(resolveInfo.activityInfo.packageName, resolveInfo.activityInfo.name);
+                break;
+            }
+        }
+
         startActivityForResult(intent, 0);
     }
 }
--- app/src/main/java/com/bloodysnow/twittercaller/TwitterCaller.java
+++ app/src/main/java/com/bloodysnow/twittercaller/TwitterCaller.java
@@ -16,8 +16,8 @@ public class TwitterCaller {
     public static final String ACTION_TWEET_SUCCESS = TwitterCaller.class.getName() + ".SUCCESS";
     public static final String ACTION_TWEET_CANCEL = TwitterCaller.class.getName() + ".CANCEL";
 
-    public void callFromUnity(final Context context, String text, final String gameObjectName, final String callbackMethodName) {
-        tweet(context, text, new Callback() {
+    public void callFromUnity(final Context context, String text, String textureUrl, final String gameObjectName, final String callbackMethodName) {
+        tweet(context, text, textureUrl, new Callback() {
             @Override
             public void onResult(int resultCode) {
                 UnityPlayer.UnitySendMessage(gameObjectName, callbackMethodName, resultCode == Activity.RESULT_OK ? "OK" : "CANCELED");
@@ -25,7 +25,7 @@ public class TwitterCaller {
         });
     }
 
-    public void tweet(Context context, String text, final Callback callback) {
+    public void tweet(Context context, String text, String textureUrl, final Callback callback) {
         final BroadcastReceiver tweetBroadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -39,6 +39,8 @@ public class TwitterCaller {
         context.registerReceiver(tweetBroadcastReceiver, intentFilter);
         Intent intent = new Intent(context, TweetActivity.class);
         intent.putExtra(Intent.EXTRA_TEXT, text);
+        if(textureUrl != null) intent.putExtra(Intent.EXTRA_STREAM, textureUrl);
+
         context.startActivity(intent);
     }
 }
