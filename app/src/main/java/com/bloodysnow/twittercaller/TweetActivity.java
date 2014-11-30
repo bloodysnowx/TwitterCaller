a1bf9bdc76fd6a853de136e02a506fd238c37a31
bloodysnowx <bloodysnowx@gmail.com>
2014-11-30 14:32:30 +0900

    Unityから呼び出すためにjarファイル化

--- app/src/main/java/com/bloodysnow/twittercaller/TweetActivity.java
+++ app/src/main/java/com/bloodysnow/twittercaller/TweetActivity.java
@@ -3,22 +3,18 @@ package com.bloodysnow.twittercaller;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
-
-import com.bloodysnow.twittercaller.R;
 
 public class TweetActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tweet);
         Intent intent = getIntent();
         tweet(intent == null ? null : intent.getStringExtra(Intent.EXTRA_TEXT));
     }
 
     @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Intent intent = new Intent(resultCode == RESULT_OK ? TwitterCaller.ACTION_TWEET_SUCCESS : TwitterCaller.ACTION_TWEET_CANCEL);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        sendBroadcast(intent);
         finish();
     }
 
--- app/src/main/java/com/bloodysnow/twittercaller/TwitterCaller.java
+++ app/src/main/java/com/bloodysnow/twittercaller/TwitterCaller.java
@@ -5,7 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
+
+import com.unity3d.player.UnityPlayer;
 
 public class TwitterCaller {
     public interface Callback {
@@ -15,18 +16,27 @@ public class TwitterCaller {
     public static final String ACTION_TWEET_SUCCESS = TwitterCaller.class.getName() + ".SUCCESS";
     public static final String ACTION_TWEET_CANCEL = TwitterCaller.class.getName() + ".CANCEL";
 
+    public void callFromUnity(final Context context, String text, final String gameObjectName, final String callbackMethodName) {
+        tweet(context, text, new Callback() {
+            @Override
+            public void onResult(int resultCode) {
+                UnityPlayer.UnitySendMessage(gameObjectName, callbackMethodName, resultCode == Activity.RESULT_OK ? "OK" : "CANCELED");
+            }
+        });
+    }
+
     public void tweet(Context context, String text, final Callback callback) {
         final BroadcastReceiver tweetBroadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                LocalBroadcastManager.getInstance(context).unregisterReceiver(this);
+                context.unregisterReceiver(this);
                 callback.onResult(intent.getAction().equals(ACTION_TWEET_SUCCESS) ? Activity.RESULT_OK : Activity.RESULT_CANCELED);
             }
         };
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(ACTION_TWEET_SUCCESS);
         intentFilter.addAction(ACTION_TWEET_CANCEL);
-        LocalBroadcastManager.getInstance(context).registerReceiver(tweetBroadcastReceiver, intentFilter);
+        context.registerReceiver(tweetBroadcastReceiver, intentFilter);
         Intent intent = new Intent(context, TweetActivity.class);
         intent.putExtra(Intent.EXTRA_TEXT, text);
         context.startActivity(intent);
